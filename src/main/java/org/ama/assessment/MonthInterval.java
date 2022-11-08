a20df8301cd14cc47b1e434887aa312efc9524a3
Peter Lane <Peter.Lane@ama-assn.org>
2022-11-08 09:28:21 -0600

    Initialize

---
+++ src/main/java/org/ama/assessment/MonthInterval.java
@@ -0,0 +1,15 @@
+package org.ama.assessment;
+
+import java.time.LocalDate;
+
+public class MonthInterval {
+	public static String[] generateMonthsInInterval(LocalDate startDate, LocalDate endDate) {
+        String[] monthNames = null;
+
+        // ### TODO: add month names generation logic here ###
+
+        // ##########################################
+
+		return monthNames;
+	 }
+}
