753f352fadee5a40a0ced327f7b933172b5c5bad
Chris Williams <chriswilliams@Chriss-MBP.lan>
2022-11-29 13:37:23 -0500

    updated assessment

--- src/main/java/org/ama/assessment/MonthInterval.java
+++ src/main/java/org/ama/assessment/MonthInterval.java
@@ -1,12 +1,51 @@
 package org.ama.assessment;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+import java.util.stream.*;
+
+
 
 public class MonthInterval {
 	public static String[] generateMonthsInInterval(LocalDate startDate, LocalDate endDate) {
         String[] monthNames = null;
 
         // ### TODO: add month names generation logic here ###
+		Period period = Period.between(startDate, endDate);
+		int years = period.getYears() * 12;
+		int months = years + period.getMonths();
+
+		monthNames = new String[months + 1];
+
+		int start = startDate.getMonth().getValue();
+
+		Map<Integer, String> monthMap = Stream.of(new Object[][]{
+				{1, "January"},
+				{2, "February"},
+				{3, "March"},
+				{4, "April"},
+				{5, "May"},
+				{6, "June"},
+				{7, "July"},
+				{8, "August"},
+				{9, "September"},
+				{10, "October"},
+				{11, "November"},
+				{12, "December"},
+		}).collect(Collectors.toMap(month -> (Integer) month[0], month -> (String) month[1]));
+
+		for (int i = 0; i <= months; i++){
+			int value = start + i;
+			if(value % 12 == 0){
+				value = 12;
+			}
+			else if(value > 12){
+				value = value % 12;
+			}
+
+			monthNames[i] = monthMap.get(value);
+		}
 
         // ##########################################
 
--- src/test/java/org/ama/assessment/MonthIntervalTest.java
+++ src/test/java/org/ama/assessment/MonthIntervalTest.java
@@ -1,11 +1,13 @@
 package org.ama.assessment;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.time.LocalDate;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 
 public class MonthIntervalTest {
@@ -25,10 +27,14 @@ public class MonthIntervalTest {
 	}
 
 	@Test
+	// expectedValue for this test should start with December
 	public void testDecember2017ToJanuary2018ReturnsDecemberThroughJanuary() {
 		LocalDate december2017= LocalDate.parse("2017-12-01");
 		LocalDate january2018 = LocalDate.parse("2018-01-01");
-        String[] expectedMonthNames = new String[] {"January", "December"};
+		// String[] expectedMonthNames = new String[] {"January", "December"};
+		// incorrect order, December 2017 should come before January 2018
+		String[] expectedMonthNames = new String[] {"December", "January"};
+
 
 		String[] monthNames = MonthInterval.generateMonthsInInterval(december2017, january2018);
         for (String monthName : monthNames) {
@@ -42,11 +48,18 @@ public class MonthIntervalTest {
 	@Test
 	public void testJanuary2017ToJanuary2018ReturnsJanuaryThroughDecember() {
 		LocalDate january2017= LocalDate.parse("2017-01-01");
-		LocalDate january2018 = LocalDate.parse("2018-03-01");
-        String[] expectedMonthNames = new String[] {
-            "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        };
+		// LocalDate january2018 = LocalDate.parse("2018-03-01");
+		// above date is March 1, 2018 but original test says January2017ToJanuary2018
+		LocalDate january2018 = LocalDate.parse("2018-01-01");
+		// String[] expectedMonthNames = new String[] {
+		// "January", "February", "March", "April", "May", "June",
+		// "July", "August", "September", "October", "November", "December"};
+		// this above array is missing January 2018
+		String[] expectedMonthNames = new String[] {
+				"January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December",
+				"January"
+		};
 
 		String[] monthNames = MonthInterval.generateMonthsInInterval(january2017, january2018);
         for (String monthName : monthNames) {
@@ -56,4 +69,75 @@ public class MonthIntervalTest {
 
 		assertEquals(expectedMonthNames, monthNames);
 	}
+
+	@Test
+	@DisplayName("should print 2 years worth of months")
+	public void shouldPrintMonthsFromJanuary2017ToJanuary2018() {
+		LocalDate january2017= LocalDate.parse("2017-01-01");
+		LocalDate january2018 = LocalDate.parse("2019-01-01");
+		String[] expectedMonthNames = new String[] {
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December",
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December",
+				"January"
+		};
+
+		String[] monthNames = MonthInterval.generateMonthsInInterval(january2017, january2018);
+		for (String monthName : monthNames) {
+			System.out.println(monthName);
+		}
+		System.out.println();
+
+		assertEquals(expectedMonthNames, monthNames);
+	}
+
+	@Test
+	@DisplayName("should print 1 month")
+	public void shouldPrintMonthFromJanuary2017ToJanuary2017() {
+		LocalDate january2017= LocalDate.parse("2017-01-01");
+		LocalDate january2018 = LocalDate.parse("2017-01-01");
+		String[] expectedMonthNames = new String[] {
+				"January"
+		};
+
+		String[] monthNames = MonthInterval.generateMonthsInInterval(january2017, january2018);
+		for (String monthName : monthNames) {
+			System.out.println(monthName);
+		}
+		System.out.println();
+
+		assertEquals(expectedMonthNames, monthNames);
+	}
+
+	@Test
+	@DisplayName("should contain 121 months")
+	public void shouldContainTheCorrectNumberOfMonthsForTenYears() {
+		LocalDate january2017= LocalDate.parse("2017-01-01");
+		LocalDate january2018 = LocalDate.parse("2027-01-01");
+		String[] monthNames = MonthInterval.generateMonthsInInterval(january2017, january2018);
+
+		assertEquals(121, monthNames.length);
+	}
+
 }
